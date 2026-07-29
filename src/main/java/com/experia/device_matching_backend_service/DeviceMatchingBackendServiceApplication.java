057dbf4c928d6de088b81026c7cbe32beaa6fdeb
Victor Manin <victormanin151@gmail.com>
2026-07-29 17:58:11 +0300

    first commit

---
+++ src/main/java/com/experia/device_matching_backend_service/DeviceMatchingBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.experia.device_matching_backend_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMatchingBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMatchingBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experia/device_matching_backend_service/DeviceMatchingBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experia.device_matching_backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMatchingBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
