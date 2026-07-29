9cd83297ba67bf2d3265ab5842a216c6acc0423e
Victor Manin <victormanin151@gmail.com>
2026-07-29 19:27:21 +0300

    added an ApiController

---
+++ src/main/java/com/experia/device_matching_backend_service/controller/ApiController.java
@@ -0,0 +1,27 @@
+package com.experia.device_matching_backend_service.controller;
+
+
+import com.experia.device_matching_backend_service.controller.dto.ApiInfoDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping
+    public ApiInfoDto ApiInfo(){
+        LocalDateTime currentTime = LocalDateTime.now();
+        return new ApiInfoDto(appName, appVersion, currentTime);
+    }
+}
