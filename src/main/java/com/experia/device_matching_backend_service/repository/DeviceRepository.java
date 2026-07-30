2b7c9dac644c9cb4b9d100f63a5d9088763f12b3
V Manin <victormanin151@gmail.com>
2026-07-30 16:38:06 +0300

    added the custom query

--- src/main/java/com/experia/device_matching_backend_service/repository/DeviceRepository.java
+++ src/main/java/com/experia/device_matching_backend_service/repository/DeviceRepository.java
@@ -3,5 +3,13 @@ package com.experia.device_matching_backend_service.repository;
 import com.experia.device_matching_backend_service.model.Device;
 import org.springframework.data.aerospike.repository.AerospikeRepository;
 
+import java.util.Optional;
+
 public interface DeviceRepository extends AerospikeRepository<Device, String> {
+    Optional<Device> findByOsNameAndOsVersionAndBrowserNameAndBrowserVersion(
+            String osName,
+            String osVersion,
+            String browserName,
+            String browserVersion
+    );
 }
