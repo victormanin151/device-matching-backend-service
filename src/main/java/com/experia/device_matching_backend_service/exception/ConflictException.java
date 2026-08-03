8b854fb14fc98d129e4c88737771fc2a0f70a463
Victor Manin <166494074+victormanin151@users.noreply.github.com>
2026-08-03 21:22:29 +0300

    Delete src/main/java/com/experia/device_matching_backend_service/exception/ConflictException.java
    not needed

--- src/main/java/com/experia/device_matching_backend_service/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.experia.device_matching_backend_service.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
